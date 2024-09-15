e21125fc31140f1794df0ddec8024fb59f6618a1
Agustín Núñez <aguunu@hotmail.com>
2024-09-14 23:59:42 -0300

    add subprocess execution

--- main.py
+++ main.py
@@ -4,12 +4,42 @@ import time
 from collections import defaultdict
 from PIL import Image
 
+import subprocess
+import ctypes
+from ctypes import wintypes
+
+# windows API constants
+SW_MAXIMIZE = 3
+
+# config for windows API
+user32 = ctypes.WinDLL("user32", use_last_error=True)
+user32.FindWindowW.restype = wintypes.HWND
+user32.FindWindowW.argtypes = [wintypes.LPCWSTR, wintypes.LPCWSTR]
+user32.ShowWindow.argtypes = [wintypes.HWND, ctypes.c_int]
+
+# open mspain
+subprocess.Popen(["mspaint"])
+
+# wait some time for mspaint to start
+time.sleep(2)
+
+# find mspaint window using default title ('Untitled - Paint')
+hwnd = user32.FindWindowW(None, "Untitled - Paint")
+
+# maximize window if found
+if hwnd:
+    user32.ShowWindow(hwnd, SW_MAXIMIZE)
+else:
+    print("MS Paint window not found.")
+
+# open input image and convert to 16 colors palette
 image = Image.open("input-image.png").reduce(2).quantize(colors=16).convert("RGB")
 
 width, height = image.size
 
-# All directions for flood fill algorithm
+# all directions for flood fill algorithm
 directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
 
 def flood_fill(image, start_coord, color, visited):
     stack = [start_coord]
@@ -40,8 +70,9 @@ for y in range(height):
             if component:
                 color_continuous_sets[color].append(component)
 
-# Custom color pixel perfect
+# mspaint custom color pixel perfect
 custom_color = (992, 70)
+
 
 def change_color(red, green, blue):
     pyautogui.moveTo(custom_color[0], custom_color[1])
@@ -56,23 +87,24 @@ def change_color(red, green, blue):
     pyautogui.press("enter")
 
 
-# Remove pyautogui default delay
+# remove pyautogui default delay
 pyautogui.PAUSE = 0.0
 
-# Screen offsets
+# screen offsets
 offset_x = 250
 offset_y = 250
 
-# Set custom delay
-component_delay = 0.000001
-line_delay = 0.000000001
+# set custom delay
+component_delay = 0.00000001
+line_delay = 0.0000000001
 
 for color, components in color_continuous_sets.items():
     print(f"Selecting color: {color}: {len(components)} pixels.")
     change_color(*color)
-    time.sleep(0.1)
+    time.sleep(component_delay)
 
-    if color == (255, 255, 255):
+    print(color)
+    if color[0] == 255 and color[1] == 255 and color[2] == 255:
         continue
 
     for component in components:
@@ -80,10 +112,10 @@ for color, components in color_continuous_sets.items():
 
         time.sleep(component_delay)
         for y in ys:
-            # Get component x-axis pixels
+            # get component x-axis pixels
             xs = [pixel[0] for pixel in component if pixel[1] == y]
 
-            # Sort x-axis pixels
+            # sort x-axis pixels
             xs.sort()
 
             x0 = x1 = xs[0]
